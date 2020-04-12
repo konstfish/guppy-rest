e7c09398722b39cbccdad8b7b90d45c8623bbff6
David Fischer <david@konst.fish>
2020-04-12 22:24:24 +0200

    add general error handling

--- guppy.py
+++ guppy.py
@@ -3,10 +3,11 @@
 
 import yaml
 import json
-
+import time
 import requests
 import sys
 
+start = time.process_time()
 print('* starting\n')
 
 def check(should_code, desc, is_code, return_data, url, cond):
@@ -19,6 +20,18 @@ def check(should_code, desc, is_code, return_data, url, cond):
 
     if(option['print_json']):
         print(json.dumps(return_data, sort_keys=True, indent=4, separators=(',', ': ')))
+
+def traverse(body):
+    for entry in body:
+        if(isinstance(body[entry], str) and body[entry].startswith('{from}')):
+            opts = body[entry].replace('{from}', '').split(':')
+            pa = opts[1].split(";")
+            tmp = storage[opts[0]]['default']
+            for i in range(len(pa)):
+                tmp = tmp[pa[i]]
+
+            body[entry] = tmp
+    return body
 
 CONFIG_NAME = './guppy.yml'
 OPTION_NAME = './guppy_options.yml'
@@ -60,16 +73,12 @@ for key in p:
 
             if('body' in p[key][location]['methods'][method]):
                 body = p[key][location]['methods'][method]['body']
+                body = traverse(body)
                 conds.append('no-body')
 
             if('headers' in p[key][location]['methods'][method]):
                 headers = p[key][location]['methods'][method]['headers']
-
-                for header in headers:
-                    if(headers[header].startswith('{from}')):
-                        opts = headers[header].replace('{from}', '').split(':')
-                        headers[header] = storage[opts[0]]['default'][opts[1]]
-
+                headers = traverse(headers)
                 conds.append('no-headers')
 
             storage[location] = {}
@@ -77,17 +86,41 @@ for key in p:
             for cond in conds:
                 if(cond == 'default'):
                     res = requests.request(method.upper(), url, data=body, headers=headers)
-                    check(p[key][location]['methods'][method]['should'][cond], p[key][location]['methods'][method]['description'], res.status_code, res.json(), url, cond)
-                    storage[location][cond] = res.json()
                 if(cond == 'no-params'):
                     pass
                 if(cond == 'no-body'):
                     res = requests.request(method.upper(), url, data={}, headers=headers)
-                    check(p[key][location]['methods'][method]['should'][cond], p[key][location]['methods'][method]['description'], res.status_code, res.json(), url, cond)
-                    storage[location][cond] = res.json()
                 if(cond == 'no-headers'):
                     res = requests.request(method.upper(), url, data=body, headers={})
-                    check(p[key][location]['methods'][method]['should'][cond], p[key][location]['methods'][method]['description'], res.status_code, res.json(), url, cond)
+
+                try:
+                    js = res.json()
                     storage[location][cond] = res.json()
+                except:
+                    print('*    - unable to decode json')
+                    js = res.text
+                    storage[location][cond] = {}
+
+                if('should' in p[key][location]['methods'][method] and cond in p[key][location]['methods'][method]['should']):
+                    should_cond = p[key][location]['methods'][method]['should'][cond]
+                else:
+                    # setting default parameters
+                    print('*  - no default parameter defined for ' + cond + '. using: ', end='')
+                    if(cond == 'no-body'):
+                        should_cond = 400
+                    elif(cond == 'no-headers'):
+                        should_cond = 401
+                    elif(cond == 'no-params'):
+                        shoud_cond = 404
+                    print(should_cond)
+
+                if('description' in p[key][location]['methods'][method]):
+                    desc = p[key][location]['methods'][method]['description']
+                else:
+                    desc = ''
+
+                check(should_cond, desc, res.status_code, js, url, cond)
+
 
             print()
+print('* done testing in ' + str( round((time.process_time() - start), 2) ) + 's')
